f948adf1d2e3426ca61e99bb9a8760c6df7695e1
EnesDemir143 <enesd41@outlook.com>
2025-02-23 16:36:02 +0300

    BottleNeck Residual block classı tamam.

--- src/resnet50Architecture/Resnet50.py
+++ src/resnet50Architecture/Resnet50.py
@@ -4,6 +4,7 @@ import torch.nn.functional as F
 
 
 class resnetStemBlock(nn.Module):
+    
     def __init__(self,in_channel,out_channel):
         super(resnetStemBlock,self).__init__()
 
@@ -20,4 +21,34 @@ class resnetStemBlock(nn.Module):
 
         return x 
     
+class resnetBottleNeckResudialBlock(nn.Module):
 
+    def __init__(self,in_channel,out_channel):
+        super(resnetBottleNeckResudialBlock,self).__init__()
+
+        self.conv1 = nn.Conv2d(in_channel,out_channel,kernel_size=1,stride=1,padding=0)
+        self.batchnorm1 = nn.BatchNorm2d()
+        
+        self.conv2 = nn.Conv2d(in_channel,out_channel,stride=1,padding=1)
+        self.batchnorm2 = nn.BatchNorm2d()
+        
+        self.conv3 = nn.Conv2d(in_channel,out_channel,kernel_size=1,stride=1,padding=0)
+        self.batchnorm3 = nn.BatchNorm2d()
+
+
+    def forward(self,x):
+        identity  =  x 
+
+        x = self.conv1(x)
+        x = F.relu(self.batchnorm1(x))
+
+        x = self.conv2(x)
+        x = F.relu(self.batchnorm2(x))
+
+        x = self.conv3(x)
+        x = F.relu(self.batchnorm3(x))
+
+        x = F.relu(torch.cat([x, identity]))
+
+        return x 
+    
